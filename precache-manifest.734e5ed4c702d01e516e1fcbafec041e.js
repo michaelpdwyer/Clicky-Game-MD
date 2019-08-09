bcd622ef9fc8e723eaeecf105d6ccf0c178a2511
Mike D <michaelp.dwyer@gmail.com>
2019-08-08 22:06:08 -0700

    Updates

---
+++ precache-manifest.734e5ed4c702d01e516e1fcbafec041e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48a8abaa32d92727283b6ef886f2079",
+    "url": "/Clicky-Game-MD/index.html"
+  },
+  {
+    "revision": "374a950fe96cc7539496",
+    "url": "/Clicky-Game-MD/static/css/main.63e82b54.chunk.css"
+  },
+  {
+    "revision": "16a9081d5ed8114b6faa",
+    "url": "/Clicky-Game-MD/static/js/2.89306916.chunk.js"
+  },
+  {
+    "revision": "374a950fe96cc7539496",
+    "url": "/Clicky-Game-MD/static/js/main.d23e2d31.chunk.js"
+  },
+  {
+    "revision": "ba01c02bc0e831e4217e",
+    "url": "/Clicky-Game-MD/static/js/runtime~main.0311a0ef.js"
+  },
+  {
+    "revision": "b7495777dd3212953f91f4e39578d235",
+    "url": "/Clicky-Game-MD/static/media/apple.b7495777.png"
+  },
+  {
+    "revision": "99e0bdc0cc390d275277158c8987a507",
+    "url": "/Clicky-Game-MD/static/media/diamond.99e0bdc0.png"
+  },
+  {
+    "revision": "0a491d03f7029fbda3a6f5a2f9a9880c",
+    "url": "/Clicky-Game-MD/static/media/dice.0a491d03.png"
+  },
+  {
+    "revision": "abfaee8b88d46a33c88bc8323696a44e",
+    "url": "/Clicky-Game-MD/static/media/feather.abfaee8b.png"
+  },
+  {
+    "revision": "22fd53810efb93326bec6284a75ac00a",
+    "url": "/Clicky-Game-MD/static/media/fire.22fd5381.png"
+  },
+  {
+    "revision": "c8aa9fe9486b2a024037c30a88452a78",
+    "url": "/Clicky-Game-MD/static/media/heart.c8aa9fe9.png"
+  },
+  {
+    "revision": "2d6f9c6f4f138bd61ad45a360d404018",
+    "url": "/Clicky-Game-MD/static/media/leaves.2d6f9c6f.png"
+  },
+  {
+    "revision": "6992598875112f9123f75fae1bc11f26",
+    "url": "/Clicky-Game-MD/static/media/lips.69925988.png"
+  },
+  {
+    "revision": "4111c0227e2713e6aa7733330f95ee2f",
+    "url": "/Clicky-Game-MD/static/media/shamrock.4111c022.png"
+  },
+  {
+    "revision": "19e9fb6b1ec8e227dafafec753e58be5",
+    "url": "/Clicky-Game-MD/static/media/star.19e9fb6b.png"
+  },
+  {
+    "revision": "807ac1099357e66dd1f65562280afd62",
+    "url": "/Clicky-Game-MD/static/media/sun.807ac109.png"
+  },
+  {
+    "revision": "47011aa924b9a71fae6f9ee60a4225cd",
+    "url": "/Clicky-Game-MD/static/media/window.47011aa9.png"
+  }
+]);
